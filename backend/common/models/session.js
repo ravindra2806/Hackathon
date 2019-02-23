0b1b66f3e0b7ecd2a814a4eaa8b7d74b08a64c2f
Kevin Jasti <kevin@arthdesignbuild.com>
2019-02-23 15:41:13 +0530

    session continuity added

--- backend/common/models/session.js
+++ backend/common/models/session.js
@@ -1,5 +1,53 @@
 'use strict';
 
-module.exports = function(Session) {
+module.exports = function (Session) {
+
+    Session.sessionStatus = function (deviceId, cb) {
+
+        Session.findOne({where: {and: [{ 'deviceId': deviceId }, {status:'open'}]}}, function (error, sessionInstance) {
+            if (error) {
+                cb(error);
+            } else {
+                if (sessionInstance) {
+                    //Get all responses for 
+                    cb(null, {
+                        sessionId: sessionInstance.id
+                    });
+                } else {
+                    //Create new sess//
+                    Session.create({ 'deviceId': deviceId, status:'open' }, function (error, session) {
+                        if (error) {
+                            cb(error)
+                        } else {
+                            Session.app.models.issueType.find({}, function (error, issueTypes) {
+                                cb(error, {
+                                    sessionId: session.id,
+                                    issueTypes: issueTypes
+                                })
+                            });
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    Session.remoteMethod(
+        'sessionStatus', {
+            accepts: [{
+                arg: 'deviceId',
+                type: 'string'
+            }],
+            http: {
+                path: '/sessionStatus/:deviceId',
+                verb: 'get'
+            },
+            returns: {
+                type: 'object',
+                root: true
+            }
+        }
+    );
+
 
 };
